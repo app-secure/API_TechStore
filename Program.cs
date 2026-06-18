86c911e02d810880eaa3a77c98ef4a5bf0456a61
Crysjurado20 <cjurado5795@uta.edu.ec>
2026-06-18 15:24:30 -0500

    desarrollo

--- Program.cs
+++ Program.cs
@@ -131,9 +131,10 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    app.UseHttpsRedirection();
+
 }
 
-app.UseHttpsRedirection();
 
 app.UseCors("AllowAnyOrigin");
 
